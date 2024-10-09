6597c99fcc94ced8d8307b6a5321c0bbcdf9a95c
unknown <davidaguilar5034@gmail.com>
2024-10-09 10:15:24 -0600

    Modificamos la conexión de la DB a localhost y eliminamos bolsa de trabajo ya que no se utiliza

--- bolsa-de-trabajo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace bolsa_de_trabajo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
